16a47753d214366f803ffd9cd2458f9f15bc6c01
gnieto <gnieto.talo@gmail.com>
2017-04-26 00:45:32 +0200

    More move operations

--- src/bytecode/mod.rs
+++ src/bytecode/mod.rs
@@ -9,6 +9,10 @@ pub enum ByteCode {
     Nop,
     Move(u8, u8),
     MoveFrom16(u8, u16),
+    Move16(u16, u16),
+    MoveWide(u8, u8),
+    MoveWideFrom16(u8, u16),
+    MoveWide16(u16, u16),
     ReturnVoid,
 }
 
@@ -22,7 +26,19 @@ impl ToString for ByteCode {
             },
             ByteCode::MoveFrom16(dest, source) => {
                 format!("move/from16 v{}, v{}", dest, source)
-            }
+            },
+            ByteCode::Move16(dest, source) => {
+                format!("move/16 v{}, v{}", dest, source)
+            },
+            ByteCode::MoveWide(dest, source) => {
+                format!("move-wide v{}, v{}", dest, source)
+            },
+            ByteCode::MoveWideFrom16(dest, source) => {
+                format!("move-wide/from16 v{}, v{}", dest, source)
+            },
+            ByteCode::MoveWide16(dest, source) => {
+                format!("move-wide/16 v{}, v{}", dest, source)
+            },
         }
     }
 }
@@ -60,6 +76,14 @@ impl<R: Read> ByteCodeDecoder<R> {
 
         Ok((dest, source))
     }
+
+    fn format32x(&mut self) -> Result<(u16, u16)> {
+        // TODO: Make byteorder generic
+        let dest = self.cursor.read_u16::<LittleEndian>()?;
+        let source = self.cursor.read_u16::<LittleEndian>()?;
+
+        Ok((dest, source))
+    }
 }
 
 impl<R: Read> Iterator for ByteCodeDecoder<R> {
@@ -77,6 +101,18 @@ impl<R: Read> Iterator for ByteCodeDecoder<R> {
             },
             Ok(0x02) => {
                 self.format22x().ok().map(|(d, s)| ByteCode::MoveFrom16(d, s))
+            },
+            Ok(0x03) => {
+                self.format32x().ok().map(|(d, s)| ByteCode::Move16(d, s))
+            },
+            Ok(0x04) => {
+                self.format12x().ok().map(|(d, s)| ByteCode::MoveWide(d, s))
+            },
+            Ok(0x05) => {
+                self.format22x().ok().map(|(d, s)| ByteCode::MoveWideFrom16(d, s))
+            },
+            Ok(0x06) => {
+                self.format32x().ok().map(|(d, s)| ByteCode::MoveWide16(d, s))
             },
             Ok(0x0e) => {
                 self.format10x().ok().map(|_| ByteCode::ReturnVoid)
@@ -132,5 +168,49 @@ mod tests {
 
         matches!(opcode, ByteCode::MoveFrom16(d, s) if d == 0xAA && s == 0x3412);
         assert_eq!("move/from16 v170, v13330", opcode.to_string());
+    }
+
+    #[test]
+    fn it_can_decode_move_16() {
+        let raw_opcode:&[u8] = &[0x03, 0xAA, 0x01, 0x12, 0x34];
+        let mut d = ByteCodeDecoder::new(raw_opcode);
+
+        let opcode = d.nth(0).unwrap();
+
+        matches!(opcode, ByteCode::Move16(d, s) if d == 0x01AA && s == 0x3412);
+        assert_eq!("move/16 v426, v13330", opcode.to_string());
+    }
+
+    #[test]
+    fn it_can_decode_move_wide() {
+        let raw_opcode:&[u8] = &[0x04, 0x3B];
+        let mut d = ByteCodeDecoder::new(raw_opcode);
+
+        let opcode = d.nth(0).unwrap();
+
+        matches!(opcode, ByteCode::MoveWide(d, s) if d == 0xB && s == 0x3);
+        assert_eq!("move-wide v3, v11", opcode.to_string());
+    }
+
+    #[test]
+    fn it_can_decode_move_wide_from_16() {
+        let raw_opcode:&[u8] = &[0x05, 0xAA, 0x12, 0x34];
+        let mut d = ByteCodeDecoder::new(raw_opcode);
+
+        let opcode = d.nth(0).unwrap();
+
+        matches!(opcode, ByteCode::MoveWideFrom16(d, s) if d == 0xAA && s == 0x3412);
+        assert_eq!("move-wide/from16 v170, v13330", opcode.to_string());
+    }
+
+    #[test]
+    fn it_can_decode_move_wide_16() {
+        let raw_opcode:&[u8] = &[0x06, 0xAA, 0x01, 0x12, 0x34];
+        let mut d = ByteCodeDecoder::new(raw_opcode);
+
+        let opcode = d.nth(0).unwrap();
+
+        matches!(opcode, ByteCode::MoveWide16(d, s) if d == 0x01AA && s == 0x3412);
+        assert_eq!("move-wide/16 v426, v13330", opcode.to_string());
     }
 }
