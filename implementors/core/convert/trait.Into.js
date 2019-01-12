77789f249f5d55e9003dd78d1c73d5df17b70bea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-12 17:31:18 +0000

    Deploy SUPERAndroidAnalyzer/dalvik to github.com/SUPERAndroidAnalyzer/dalvik.git:gh-pages

--- implementors/core/convert/trait.Into.js
+++ implementors/core/convert/trait.Into.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["backtrace"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/vec/struct.Vec.html\" title=\"struct alloc::vec::Vec\">Vec</a>&lt;<a class=\"struct\" href=\"backtrace/struct.BacktraceFrame.html\" title=\"struct backtrace::BacktraceFrame\">BacktraceFrame</a>&gt;&gt; for <a class=\"struct\" href=\"backtrace/struct.Backtrace.html\" title=\"struct backtrace::Backtrace\">Backtrace</a>",synthetic:false,types:["backtrace::capture::Backtrace"]},];
+implementors["dalvik"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"enum\" href=\"https://doc.rust-lang.org/nightly/core/option/enum.Option.html\" title=\"enum core::option::Option\">Option</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u32.html\">u32</a>&gt;&gt; for <a class=\"enum\" href=\"dalvik/read/enum.U32p1.html\" title=\"enum dalvik::read::U32p1\">U32p1</a>",synthetic:false,types:["dalvik::read::U32p1"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
