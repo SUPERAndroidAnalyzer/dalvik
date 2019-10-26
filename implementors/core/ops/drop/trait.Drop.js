f59c5579b1dde3e227c259edaeb95615c4a0a27d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-26 13:18:06 +0000

    Deploy SUPERAndroidAnalyzer/dalvik to github.com/SUPERAndroidAnalyzer/dalvik.git:gh-pages

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["backtrace"] = [{text:"impl&lt;'_, '_, '_&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"backtrace/struct.BacktraceFrameFmt.html\" title=\"struct backtrace::BacktraceFrameFmt\">BacktraceFrameFmt</a>&lt;'_, '_, '_&gt;",synthetic:false,types:["backtrace::print::BacktraceFrameFmt"]},];
 implementors["syn"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"syn/parse/struct.ParseBuffer.html\" title=\"struct syn::parse::ParseBuffer\">ParseBuffer</a>&lt;'a&gt;",synthetic:false,types:["syn::parse::ParseBuffer"]},];
 
             if (window.register_implementors) {
